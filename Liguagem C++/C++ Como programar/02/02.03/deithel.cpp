5c19efc9038a9cea3a646e70c80bf0189b8cdbbe
EDUARDO FELIZARDO CANDIDO <eduardo.candido@fatec.sp.gov.br>
2018-11-30 15:00:12 -0200

    R00

---
+++ Liguagem C++/C++ Como programar/02/02.03/deithel.cpp
@@ -0,0 +1,9 @@
+/*Escreva uma única instrução C++ para realizar cada uma das seguintes tarefas (suponha que as declarações u ing não foram utilizadas): 
+    a) Declare as variáveis c, thisIsAVariable ,q76354 enumber como sendo do tipoString .
+    b) Solicite que o usuário insira um inteiro. Termine sua mensagem de solicitação com dois-pontos 
+    c) Leia um inteiro do usuário no teclado e armazene o valor inserido na variável do tipo inteiro cursor posicionado depois do espaço. age. 
+    d) Se a variávelnumber não for igual a7, imprima“The variable number is not equal to 7” .
+    e) Imprima a mensagem“This is a C++ program” em uma linha. 
+    f) Imprima a mensagem“This is a C++ program” em duas linhas. Termine a primeira linha com C++.
+    g) Imprima a mensagem“This is a C++ program” com cada palavra em uma linha separada. 
+    h) Imprima a mensagem“This is a C++ program” com cada palavra separada da palavra seguinte por uma tabulação.*/
